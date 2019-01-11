da0e32faf86028b62308f10db45052f49c6b546c
Matthew <matthew.paul.welch@gmail.com>
2019-01-11 12:41:16 +1030

    [develop] Begin setting colours for different divisions/areas

--- js/district-club-map.js
+++ js/district-club-map.js
@@ -51,6 +51,7 @@ function getClubInfo() {
 
 function clubMap() {
     if (clubInfo !== undefined) {
+        initialiseColours();
         var mapCenter = getCenter(clubInfo);
         var mapProperties = {
             center: new google.maps.LatLng(mapCenter.latitude, mapCenter.longitude),
@@ -69,7 +70,14 @@ function addClubMarkers(map) {
     for (var i = 0; i < clubInfo.Clubs.length; i++) {
         new google.maps.Marker({
             position: new google.maps.LatLng(clubInfo.Clubs[i].Address.Coordinates.Latitude,
-                                             clubInfo.Clubs[i].Address.Coordinates.Longitude)
+                                             clubInfo.Clubs[i].Address.Coordinates.Longitude),
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10,
+                fillColor: getColour(clubInfo.Clubs[i]),
+                fillOpacity: 0.6,
+                strokeOpacity: 0.0,
+            },
         }).setMap(map);
     }
 }
@@ -78,4 +86,30 @@ function addClubMarkers(map) {
 function getCenter(clubInfo) {
     //TODO: implement this for the given club info
     return { latitude: -38, longitude: 142 };
+}
+
+
+function getColour(club) {
+    getDivisionHue(club);
+    getAreaLightness(club);
+}
+
+clubColours
+function initialiseColours() {
+    if (clubInfo == undefined) {
+        log('Cannot initialise colours - no club data');
+        return;
+    }
+    var divisions = new Map();
+    var areas = new Map();
+    for (var i = 0; i < clubInfo.Clubs.length; i++) {
+        areas.set(clubInfo.Clubs[0].Classification.Area.name, '');
+        divisions.set(clubInfo.Clubs[0].Classification.Division.name, '');
+    }
+    //count divisions
+        //pick a hue for each divisions
+    //count areas in each division
+        //pick a lightness for each area
+    // result is a populated list of Map<Division, hue> and Map<Area, saturation>
+
 }
